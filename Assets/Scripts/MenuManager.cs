057daaf02b83f0d0f30a3e68b07f269dcd29265f
Robowizdom <robowizdom@gmail.com>
2022-11-26 10:12:14 +0100

    Updated UI
    Added main menu ui and options UI

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [Header("Main Menu")]
+    public Button startBtn, optionsBtn, quitBtn;
+    [Header("Panel")]
+    public GameObject menuPanel, optionPanel;
+    [Header("Options")]
+    public AudioSource audioS;
+    public Button backBtn;
+    public Slider volSlide;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startBtn.onClick.AddListener(StartGame);
+        quitBtn.onClick.AddListener(QuitGame);
+        optionsBtn.onClick.AddListener(OpenOptions);
+        backBtn.onClick.AddListener(CloseOptions);
+        volSlide.onValueChanged.AddListener(UpdateVolume);
+    }
+
+    // Update is called once per frame
+    void OpenOptions()
+    {
+        optionPanel.SetActive(true);
+        menuPanel.SetActive(false);
+    }
+
+    void CloseOptions()
+    {
+        menuPanel.SetActive(true);
+        optionPanel.SetActive(false);
+    }
+
+    void UpdateVolume(float val)
+    {
+        val = volSlide.value;
+        audioS.volume = val;
+    }
+
+    void StartGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    void QuitGame()
+    {
+        Application.Quit();
+    }
+}
