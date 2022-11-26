911a11768997da8a6b98ab5eaf3ee8d74b1fa142
princehalo <princehalo@ymail.com>
2022-11-26 17:29:06 +0100

    Spawn Manager added

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    // public GameObject[] vehiclePrefabs;
+    // private float spawnRangeX = 318.4f;
+    // private float spawnPosZ = 4.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if (Input.GetKeyDown(KeyCode.S)) {
+        //     //Randomly generated animal index and spawn position
+        //     Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),
+        //     0, spawnPosZ);
+        //     int vehicleIndex = Random.Range(0, vehiclePrefabs.Lenght);
+        //     Instantiate(vehiclePrefabs[vehicleIndex], spawnPos,
+        //     vehiclePrefabs[vehicleIndex].transform.rotation);
+        // }
+    }
+}
